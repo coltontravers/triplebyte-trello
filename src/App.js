8867b79d91f4000f8f631b8aa415709fd81332d4
Colton Travers <contactcolt@gmail.com>
2018-03-14 15:52:58 -0500

    Some changes.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
 import React, { Component } from 'react';
-<<<<<<< HEAD
-import Boards from './components/Boards/Boards.js';
-import logo from './logo.svg';
-=======
 import BoardContainer from './BoardContainer/BoardContainer.js';
 import Board from './Board/Board.js';
->>>>>>> 7319b4dd642cceb30949e4a8ccef84547a3692f9
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-<<<<<<< HEAD
-        <Boards/>
-=======
       <BoardContainer></BoardContainer>
->>>>>>> 7319b4dd642cceb30949e4a8ccef84547a3692f9
       </div>
     );
   }
